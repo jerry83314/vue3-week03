d928f73e3c81cb28fc2c2c3b05fe84d183b9c3e1
Jerry Huang <jerry83314@gmail.com>
2023-01-12 22:40:24 +0800

    串接新增產品 API

--- js/product.js
+++ js/product.js
@@ -2,16 +2,30 @@ const { createApp, ref, onMounted } = Vue;
 
 createApp({
   setup() {
+    const api_url = ref('https://vue3-course-api.hexschool.io');
+    const api_path = ref('jyue-web');
     const products = ref([]);
+    const newProduct = ref({
+      title: '',
+      category: '',
+      origin_price: 0,
+      price: 0,
+      unit: '',
+      description: '',
+      content: '',
+      is_enabled: 1,
+      imageUrl: '',
+      imagesUrl: []
+    });
 
     onMounted(() => {
-      checkLogin()
+      checkLogin();
     })
 
     // 驗證是否登入
     function checkLogin() {
       // 取得 token
-      const token = document.cookie.replace(/(?:(?:^|.*;\s*)loginToken\s*=\s*([^;]*).*$)|^.*$/,"$1");
+      const token = document.cookie.replace(/(?:(?:^|.*;\s*)loginToken\s*=\s*([^;]*).*$)|^.*$/, "$1");
       // headers 帶上 token
       axios.defaults.headers.common['Authorization'] = token;
 
@@ -30,13 +44,28 @@ createApp({
 
     // 取得產品列表
     function getProduct() {
-      const url = 'https://vue3-course-api.hexschool.io';
-      const api_path = 'jyue-web'
-
-      axios.get(`${url}/v2/api/${api_path}/admin/products/all`)
+      axios.get(`${api_url.value}/v2/api/${api_path.value}/admin/products/all`)
         .then((res) => {
+          
           products.value = Object.values(res.data.products);
-          temp.value = products[0];
+          console.log('取得產品', products.value)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+
+    // 新增產品
+    function addProduct() {
+      const data = {
+        data: newProduct.value
+      }
+      axios.post(`${api_url.value}/v2/api/${api_path.value}/admin/product`, data)
+        .then((res) => {
+          // 關閉 modal，但關不起來 ？
+          const myModal = new bootstrap.Modal(document.getElementById('productModal'), {});
+          myModal.hide();
+          getProduct();
         })
         .catch((err) => {
           console.log(err)
@@ -50,9 +79,13 @@ createApp({
     }
 
     return {
-      getProduct,
-      checkLogin,
+      api_url,
+      api_path,
+      newProduct,
       products,
+      checkLogin,
+      getProduct,
+      addProduct,
       openModal
     }
   }
